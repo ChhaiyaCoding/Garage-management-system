4cca362c921b805c294163ec8584e9cf6035c006
ChhaiyaCoding <chhaiyanpic@gmail.com>
2026-06-03 19:36:54 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import CustomerPortal from './pages/CustomerPortal'
 
-createRoot(document.getElementById('root')).render(<App />)
+const isPortal = window.location.pathname.startsWith('/portal')
+
+if (isPortal) {
+  createRoot(document.getElementById('root')).render(<CustomerPortal />)
+} else {
+  createRoot(document.getElementById('root')).render(<App />)
+}
